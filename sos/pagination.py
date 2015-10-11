77ce7275842e36d5f0a5a663a0f7366814551c81
Josh Levinger <josh@levinger.net>
2015-10-10 19:01:24 -0700

    missing import

--- sos/pagination.py
+++ sos/pagination.py
@@ -5,3 +5,9 @@ class LargeResultsSetPagination(pagination.PageNumberPagination):
     page_size = 1000
     page_size_query_param = 'page_size'
     max_page_size = 10000
+
+
+class MediumResultsSetPagination(pagination.PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 100
