0e49a25b91e607ec8f649b555f22e29436b5cceb
Josh Levinger <josh@levinger.net>
2015-10-10 18:38:28 -0700

    increase api default page_size

--- sos/settings.py
+++ sos/settings.py
@@ -100,7 +100,7 @@ CORS_ALLOW_HEADERS = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 25,
+    'PAGE_SIZE': 50,
     'DEFAULT_PERMISSION_CLASSES': (
         'sos.permissions.IsAuthorOrReadOnly',
     )
