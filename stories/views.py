43e238a7e54aa63831cbf7e68e5b43daebe7a7c9
Josh Levinger <josh@levinger.net>
2015-11-23 15:06:45 -0800

    state abbreviations, don't replace space to underscore

--- stories/views.py
+++ stories/views.py
@@ -9,7 +9,7 @@ from serializers import (StorySerializer, LocationStoriesSerializer,
     StateStoriesSerializer, CountyStoriesSerializer, ZipcodeStoriesSerializer)
 
 from localflavor.us.us_states import US_STATES
-STATE_ABBRS = {v.replace(' ', '_'): k for k, v in US_STATES}  # convert state names to map format
+STATE_ABBRS = {v: k for k, v in US_STATES}  # convert state names to map format
 
 
 class StoryViewSet(viewsets.ModelViewSet):
